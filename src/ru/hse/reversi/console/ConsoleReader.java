3878e96c939575ea70df8fc7ccef743409366c76
Ilya <KobyzevIlya@gmail.com>
2023-02-24 18:56:10 +0300

    fix of previous fix

--- src/ru/hse/reversi/console/ConsoleReader.java
+++ src/ru/hse/reversi/console/ConsoleReader.java
@@ -7,9 +7,7 @@ public class ConsoleReader {
 
     public static String getCommand() {
         Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
-        scanner.close();
 
-        return command;
+        return scanner.nextLine();
     }
 }
